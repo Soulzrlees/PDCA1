87206e15db08f29c26e2bcdce01eb9095a29754c
Fateh Bhular <fatehbhular@gmail.com>
2025-07-27 17:06:37 +1200

    Testing commit, push, and pull.

--- src/main/main.java
+++ src/main/main.java
@@ -9,6 +9,7 @@ package main;
  * @author fatehbhular
  */
 public class main {
-    //comment
-    
+    public static void main(String[] args) {
+        System.out.println("Hello Shawn");
+    }
 }
