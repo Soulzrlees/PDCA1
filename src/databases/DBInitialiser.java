768d43ed84bfa82600d323bcbc1d33e5a3e78ec7
Fateh Bhular <fatehbhular@gmail.com>
2025-10-08 16:02:08 +1300

    Created Tables in the databases

---
+++ src/databases/DBInitialiser.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package databases;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author fatehbhular
+ */
+public class DBInitialiser {
+    private static final String AccountsDB_URL = "jdbc:derby:/Users/fatehbhular/NetBeansProjects/P12_23217987_23204035/ACCOUNTDATABASE_Ebd;create=true";
+    private static final String StatsDB_URL = "jdbc:derby:/Users/fatehbhular/NetBeansProjects/P12_23217987_23204035/STATSDATABASE_Ebd;create=true";
+    
+    public static void main(String[] args) {
+        System.out.println("Initialising Database...");
+        initialiseAccountsDB();
+        initialiseStatsDB();
+        System.out.println("Initialisation complete!");
+    }
+    
+    public static void initialiseAccountsDB() {
+        DBManager dbManager = new DBManager(AccountsDB_URL, "pdc", "pdc");
+        
+        try {
+            Connection conn = dbManager.getConnection();
+            Statement statement = conn.createStatement();
+            
+            String sql = """
+                         CREATE TABLE ACCOUNTS (
+                            USERNAME VARCHAR(30) PRIMARY KEY, 
+                            LEVEL INT, 
+                            EXP INT, 
+                            GOLD INT, 
+                            CLASS VARCHAR(20)
+                         )
+                         """;
+            statement.executeUpdate(sql);
+            conn.commit();
+            System.out.println("Table created successfully!");
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println("Accounts table was not created: " + e.getMessage());
+        } finally {
+            dbManager.close();
+        }
+    }
+    
+    public static void initialiseStatsDB() {
+        DBManager dbManager = new DBManager(StatsDB_URL, "pdc", "pdc");
+        
+        try {
+            Connection conn = dbManager.getConnection();
+            Statement statement = conn.createStatement();
+            
+            String sql = """
+                         CREATE TABLE STATS (
+                            USERNAME VARCHAR(30) PRIMARY KEY,
+                            DAMAGE_POINTS INT, 
+                            HEALTH_POINTS INT,
+                            RANGE_POINTS INT
+                         )
+                         """;
+            statement.executeUpdate(sql);
+            conn.commit();
+            System.out.println("Table created successfully!");
+            statement.close();
+        } catch (SQLException e) {
+            System.err.println("Stats table was not created: " + e.getMessage());
+        } finally {
+            dbManager.close();
+        }
+    }
+}
